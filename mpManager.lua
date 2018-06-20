bb40a65586545747abf855ebba100bcbf6bd1974
kevink98 <kevin.knoeller@live.de>
2018-06-20 11:35:45 +0200

    update version

--- mpManager.lua
+++ mpManager.lua
@@ -3,12 +3,12 @@
 -- 
 -- @Interface: 1.5.3.1 b1841
 -- @Author: LS-Modcompany/kevink98 
--- @Date: 18.06.2018
--- @Version: 1.0.0.2
+-- @Date: 20.06.2018
+-- @Version: 1.0.0.3
 -- 
 -- @Support: LS-Modcompany
 -- 
-local version = "1.0.0.2 (18.06.2018)";
+local version = "1.0.0.3 (20.06.2018)";
 
 MpManager = {};
 --_G["g_mpManager"] = MpManager;
